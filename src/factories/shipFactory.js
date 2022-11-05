60e6e121a80a8aea22ec28e2cf71012a521e52ce
guihi1 <gui.hikio@gmail.com>
2022-11-05 15:46:30 -0300

    Add coord to returned functions

--- src/factories/shipFactory.js
+++ src/factories/shipFactory.js
@@ -1,10 +1,10 @@
 const shipFactory = (length, hits, sunk, coord = []) => {
-  const hit = () => shipFactory(length, hits + 1, sunk);
+  const hit = () => shipFactory(length, hits + 1, sunk, coord);
   const isSunk = () => {
     if (hits === length) {
-      return shipFactory(length, hits, true);
+      return shipFactory(length, hits, true, coord);
     }
-    return shipFactory(length, hits, false);
+    return shipFactory(length, hits, false, coord);
   };
   return {
     length, hits, sunk, coord, hit, isSunk,
