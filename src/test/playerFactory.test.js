1111052a10e2b8a32e6f7f1483ec7e28e63d217b
guihi1 <gui.hikio@gmail.com>
2022-11-11 19:34:33 -0300

    Add one more check

--- src/test/playerFactory.test.js
+++ src/test/playerFactory.test.js
@@ -1,4 +1,4 @@
-import playerFactory from "../factories/playerFactory";
+import playerFactory from '../factories/playerFactory';
 
 const player1 = playerFactory(true);
 
@@ -9,4 +9,8 @@ test('Changing turns', () => {
 test('Computer turn', () => {
   expect(player1.computerTurn()).toBeGreaterThanOrEqual(0);
   expect(player1.computerTurn()).toBeLessThan(100);
+  for (let i = 0; i < 99; i += 1) {
+    player1.compVisited.push(i);
+  }
+  expect(player1.computerTurn()).toBe(99);
 });
