c32edd9f32857042f4c97ffd9832bb7edc3a456d
guihi1 <gui.hikio@gmail.com>
2022-12-06 20:31:42 -0300

    Add function newGame

--- src/DOM/addPlayerEvents.js
+++ src/DOM/addPlayerEvents.js
@@ -1,10 +1,32 @@
 import boardFactory from '../factories/boardFactory';
 import playerFactory from '../factories/playerFactory';
+import gameEnd from './gameEnd';
 
 let turn = true;
 const player = playerFactory(turn);
 const playerBoardSquares = document.querySelectorAll('.row > div');
 const playerBoard = boardFactory();
+const mask = document.getElementById('page-mask');
+
+const newGame = () => {
+  const playAgain = document.querySelector('.restart');
+  playAgain.addEventListener('click', () => {
+    playerBoard.boardReset();
+    player.playerReset();
+    mask.classList.remove('show');
+    mask.classList.add('hide');
+    for (let i = 0; i < playerBoardSquares.length; i += 1) {
+      playerBoardSquares[i].classList.remove('hit');
+      playerBoardSquares[i].classList.remove('missed');
+    }
+
+    const compBoardSquares = document.querySelectorAll('.row-c > div');
+    for (let j = 0; j < compBoardSquares.length; j += 1) {
+      compBoardSquares[j].classList.remove('hit');
+      compBoardSquares[j].classList.remove('missed');
+    }
+  });
+};
 
 //  Add event listeners to player board
 const addPlayerEvents = () => {
@@ -14,6 +36,10 @@ const addPlayerEvents = () => {
     playerBoardSquares[playerIndex].classList.add('missed');
   } else if (playerBoard.board[playerIndex] === 'H') {
     playerBoardSquares[playerIndex].classList.add('hit');
+  }
+  if (playerBoard.allSunk()) {
+    gameEnd('loss');
+    newGame();
   }
 };
 
