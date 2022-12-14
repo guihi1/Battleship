04510b507ce7fbb8a67ed2571e0e0dd2fb62fb3c
guihi1 <gui.hikio@gmail.com>
2022-12-14 15:00:28 -0300

    Change what the function receives

--- src/DOM/addCompEvents.js
+++ src/DOM/addCompEvents.js
@@ -4,30 +4,11 @@ import gameEnd from './gameEnd';
 
 const compBoardSquares = document.querySelectorAll('.row-c > div');
 const compBoard = boardFactory();
-const mask = document.querySelector('#page-mask');
 const hasEvents = [];
-
-const newGame = () => {
-  const playAgain = document.querySelector('.restart');
-  playAgain.addEventListener('click', () => {
-    compBoard.boardReset();
-    mask.classList.remove('show');
-    mask.classList.add('hide');
-    for (let i = 0; i < compBoardSquares.length; i += 1) {
-      compBoardSquares[i].classList.remove('hit');
-      compBoardSquares[i].classList.remove('missed');
-    }
-
-    const playerBoardSquares = document.querySelectorAll('.row > div');
-    for (let j = 0; j < playerBoardSquares.length; j += 1) {
-      playerBoardSquares[j].classList.remove('hit');
-      playerBoardSquares[j].classList.remove('missed');
-    }
-  });
-};
 
 const onClick = (evt) => {
   compBoard.receiveAttack(evt.currentTarget.index);
+  evt.currentTarget.visited.push(evt.currentTarget.index);
   if (compBoard.board[evt.currentTarget.index] === 'M') {
     evt.currentTarget.classList.add('missed');
   } else if (compBoard.board[evt.currentTarget.index] === 'H') {
@@ -37,26 +18,22 @@ const onClick = (evt) => {
   hasEvents[evt.currentTarget.index] = false;
   if (compBoard.allSunk()) {
     gameEnd('win');
-    newGame();
   }
   addPlayerEvents();
 };
 
 //  Add event listeners to computer board
-function addCompEvents(visited) {
-  console.log('fkajdkfajl');
+function addCompEvents(player) {
   for (let i = 0; i < compBoardSquares.length; i += 1) {
     if (hasEvents[i]) {
       compBoardSquares[i].removeEventListener('click', onClick);
       hasEvents[i] = false;
     }
-
-    if (!visited.includes(i)) {
-      console.log('giajrodijafçoid');
+    if (!player.visited.includes(i)) {
       hasEvents[i] = true;
       compBoardSquares[i].addEventListener('click', onClick);
       compBoardSquares[i].index = i;
-      visited.push(i);
+      compBoardSquares[i].visited = player.visited;
     }
   }
 }
