062a5ce018d30f26a06888a55cb7109301c8aa32
guihi1 <gui.hikio@gmail.com>
2022-12-15 19:54:48 -0300

    Add event listener to buttons

--- src/index.js
+++ src/index.js
@@ -2,16 +2,51 @@ import './styles.css';
 import addCompEvents, { compBoard } from './DOM/addCompEvents';
 import { playerBoard, player } from './DOM/addPlayerEvents';
 
-playerBoard.placeShips(5, 0, 'horizontal');
-playerBoard.placeShips(4, 31, 'vertical');
-playerBoard.placeShips(3, 50, 'horizontal');
-playerBoard.placeShips(3, 73, 'vertical');
-playerBoard.placeShips(2, 89, 'horizontal');
+const startPage = document.getElementById('start-page');
+const start = document.getElementById('start');
+start.addEventListener('click', () => {
+  startPage.classList.add('hide');
+  const carPos = document.getElementById('car-square').value;
+  const carDir = document.getElementById('car-dir').value;
+  playerBoard.placeShips(5, carPos, carDir);
+  const batPos = document.getElementById('bat-square').value;
+  const batDir = document.getElementById('bat-dir').value;
+  playerBoard.placeShips(4, batPos, batDir);
+  const cruPos = document.getElementById('cru-square').value;
+  const cruDir = document.getElementById('cru-dir').value;
+  playerBoard.placeShips(3, cruPos, cruDir);
+  const subPos = document.getElementById('sub-square').value;
+  const subDir = document.getElementById('sub-dir').value;
+  playerBoard.placeShips(3, subPos, subDir);
+  const desPos = document.getElementById('des-square').value;
+  const desDir = document.getElementById('des-dir').value;
+  playerBoard.placeShips(2, desPos, desDir);
+});
 
-compBoard.placeShips(5, 3, 'horizontal');
-compBoard.placeShips(4, 31, 'horizontal');
-compBoard.placeShips(3, 49, 'vertical');
-compBoard.placeShips(3, 61, 'vertical');
-compBoard.placeShips(2, 85, 'horizontal');
+compBoard.placeShips(5, 0);
+compBoard.placeShips(4, 1);
+compBoard.placeShips(3, 2);
+compBoard.placeShips(3, 3);
+compBoard.placeShips(2, 4);
 
-addCompEvents(player.visited);
+addCompEvents(player);
+
+const playAgain = document.getElementById('restart');
+playAgain.addEventListener('click', () => {
+  player.playerReset();
+  playerBoard.boardReset();
+  compBoard.boardReset();
+  playerBoard.placeShips(5, 0, 'horizontal');
+  playerBoard.placeShips(4, 31, 'vertical');
+  playerBoard.placeShips(3, 50, 'horizontal');
+  playerBoard.placeShips(3, 73, 'vertical');
+  playerBoard.placeShips(2, 89, 'horizontal');
+
+  compBoard.placeShips(5, 0);
+  compBoard.placeShips(4, 1);
+  compBoard.placeShips(3, 2);
+  compBoard.placeShips(3, 3);
+  compBoard.placeShips(2, 4);
+  console.log(player);
+  addCompEvents(player);
+});
