3d8bb5a59d4f56a6de3c5da33399bc451fdbb954
guihi1 <gui.hikio@gmail.com>
2022-12-16 19:37:09 -0300

    Change how missed attacks are displayed

--- src/DOM/addPlayerEvents.js
+++ src/DOM/addPlayerEvents.js
@@ -7,20 +7,32 @@ const player = playerFactory(turn);
 const playerBoardSquares = document.querySelectorAll('.row > div');
 const playerBoard = boardFactory();
 const mask = document.getElementById('page-mask');
+const start = document.getElementById('start-page');
+
+for (let i = 0; i < playerBoardSquares.length; i += 1) {
+  playerBoardSquares[i].classList.add('default');
+}
 
 const playAgain = document.querySelector('.restart');
 playAgain.addEventListener('click', () => {
   mask.classList.remove('show');
   mask.classList.add('hide');
+  start.classList.remove('hide');
   for (let i = 0; i < playerBoardSquares.length; i += 1) {
     playerBoardSquares[i].classList.remove('hit');
-    playerBoardSquares[i].classList.remove('missed');
+    playerBoardSquares[i].textContent = '';
+    if (!playerBoardSquares[i].classList.contains('default')) {
+      playerBoardSquares[i].classList.add('default');
+    }
   }
 
   const compBoardSquares = document.querySelectorAll('.row-c > div');
   for (let j = 0; j < compBoardSquares.length; j += 1) {
     compBoardSquares[j].classList.remove('hit');
-    compBoardSquares[j].classList.remove('missed');
+    compBoardSquares[j].textContent = '';
+    if (!compBoardSquares[j].classList.contains('default')) {
+      compBoardSquares[j].classList.add('default');
+    }
   }
 });
 
@@ -28,11 +40,11 @@ playAgain.addEventListener('click', () => {
 const addPlayerEvents = () => {
   const playerIndex = player.computerTurn();
   playerBoard.receiveAttack(playerIndex);
-  if (playerBoard.board[playerIndex] === 'M') {
-    playerBoardSquares[playerIndex].classList.add('missed');
-  } else if (playerBoard.board[playerIndex] === 'H') {
+  if (playerBoard.board[playerIndex] === 'H') {
     playerBoardSquares[playerIndex].classList.add('hit');
+    playerBoardSquares[playerIndex].classList.remove('default');
   }
+  playerBoardSquares[playerIndex].textContent = '✖';
   if (playerBoard.allSunk()) {
     gameEnd('loss');
   }
