e75658b45508fa7536be3ad177d9ff2ed8901ca2
guihi1 <gui.hikio@gmail.com>
2022-12-17 19:48:22 -0300

    Add number validation

--- src/index.js
+++ src/index.js
@@ -5,22 +5,61 @@ import { playerBoard, player } from './DOM/addPlayerEvents';
 const startPage = document.getElementById('start-page');
 const start = document.getElementById('start');
 start.addEventListener('click', () => {
-  startPage.classList.add('hide');
   const carPos = document.getElementById('car-square').value;
   const carDir = document.getElementById('car-dir').value;
-  playerBoard.placeShips(5, Number(carPos), carDir);
   const batPos = document.getElementById('bat-square').value;
   const batDir = document.getElementById('bat-dir').value;
-  playerBoard.placeShips(4, Number(batPos), batDir);
   const cruPos = document.getElementById('cru-square').value;
   const cruDir = document.getElementById('cru-dir').value;
-  playerBoard.placeShips(3, Number(cruPos), cruDir);
   const subPos = document.getElementById('sub-square').value;
   const subDir = document.getElementById('sub-dir').value;
-  playerBoard.placeShips(3, Number(subPos), subDir);
   const desPos = document.getElementById('des-square').value;
   const desDir = document.getElementById('des-dir').value;
-  playerBoard.placeShips(2, Number(desPos), desDir);
+  if (
+    carPos < 0 || carPos > 99
+    || batPos < 0 || batPos > 99
+    || cruPos < 0 || cruPos > 99
+    || subPos < 0 || subPos > 99
+    || desPos < 0 || desPos > 99
+  ) {
+    const carField = document.getElementById('car-field');
+    const batField = document.getElementById('bat-field');
+    const cruField = document.getElementById('cru-field');
+    const subField = document.getElementById('sub-field');
+    const desField = document.getElementById('des-field');
+    if (carPos < 0 || carPos > 99) {
+      carField.classList.add('validation-error');
+    } else {
+      carField.classList.add('validation-success');
+    }
+    if (batPos < 0 || batPos > 99) {
+      batField.classList.add('validation-error');
+    } else {
+      batField.classList.add('validation-success');
+    }
+    if (cruPos < 0 || cruPos > 99) {
+      cruField.classList.add('validation-error');
+    } else {
+      cruField.classList.add('validation-success');
+    }
+    if (subPos < 0 || subPos > 99) {
+      subField.classList.add('validation-error');
+    } else {
+      subField.classList.add('validation-success');
+    }
+    if (desPos < 0 || desPos > 99) {
+      desField.classList.add('validation-error');
+    } else {
+      desField.classList.add('validation-success');
+    }
+  } else {
+    playerBoard.placeShips(5, Number(carPos), carDir);
+    playerBoard.placeShips(4, Number(batPos), batDir);
+    playerBoard.placeShips(3, Number(cruPos), cruDir);
+    playerBoard.placeShips(3, Number(subPos), subDir);
+    playerBoard.placeShips(2, Number(desPos), desDir);
+    startPage.classList.add('hide');
+  }
 });
 
 compBoard.placeShips(5, 0);
